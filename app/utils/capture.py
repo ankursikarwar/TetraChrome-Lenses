2330290aca9671cde6e234854fcef1eb852311df
Ankur Sikarwar <ankursikarwardc@gmail.com>
2019-09-10 23:43:39 +0530

    Update capture.py

--- app/utils/capture.py
+++ app/utils/capture.py
@@ -3,9 +3,9 @@ import os
 def image_capture_and_save(HighResolution = True):
     print("Capture requested")
     if (HighResolution == True):
-        os.system('sh /app/utils/high_res_capture.sh')
+        os.system('sh ./app/utils/high_res_capture.sh')
     if (HighResolution == False):
-        os.system('sh /app/utils/low_res_capture.sh')
+        os.system('sh ./app/utils/low_res_capture.sh')
     if (len(os.listdir('./data/capture')) == 0):
         return False
     else:
